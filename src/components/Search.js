b3f39b1bbe7382ec5a4597988f0209c3ee147fcf
Phuthuan1101 <thuan110199@gmail.com>
2024-06-29 11:33:42 +0700

    frist commit

---
+++ src/components/Search.js
@@ -0,0 +1,88 @@
+import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import Book from './Book'
+import * as BooksAPI from '../BooksAPI'
+import useQuery from '../Hook/useQuery'
+import '../App.css'
+
+
+
+const Search = () => {
+
+    const [books, setBooks] = useState([])
+  const [mapOfIdToBooks, setMapOfIdToBooks] = useState(new Map());
+
+  const [query, setQuery] = useState("");
+  const [searchBooks] = useQuery(query);
+  const [mergedBooks, setMergedBooks] = useState([]);
+
+  useEffect(() => {
+
+    BooksAPI.getAll()
+      .then(data => {
+        setBooks(data)
+        setMapOfIdToBooks(createMapOfBooks(data))
+      }
+      );
+  }, [])
+
+
+  useEffect(() => {
+
+    const combined = searchBooks.map(book => {
+      if (mapOfIdToBooks.has(book.id)) {
+        return mapOfIdToBooks.get(book.id);
+      } else {
+        return book;
+      }
+    })
+    setMergedBooks(combined);
+  }, [searchBooks])
+
+
+  const createMapOfBooks = (books) => {
+    const map = new Map();
+    books.map(book => map.set(book.id, book));
+    return map;
+  }
+
+  const updateBookShelf = (book, whereTo) => {
+    const updatedBooks = books.map(b => {
+      if (b.id === book.id) {
+        book.shelf = whereTo;
+        return book;
+      }
+      return b;
+    })
+    if (!mapOfIdToBooks.has(book.id)) {
+      book.shelf = whereTo;
+      updatedBooks.push(book)
+    }
+    setBooks(updatedBooks);
+    BooksAPI.update(book, whereTo);
+  }
+
+    return (
+        <div className="search-books">
+              <div className="search-books-bar">
+                <Link to="/">
+                  <button className="close-search">Close</button>
+                </Link>
+                <div className="search-books-input-wrapper">
+                  <input type="text" placeholder="Search by title or author" value={query} onChange={(e) => setQuery(e.target.value)} />
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid">
+                  {mergedBooks.map(b => (
+                    <li key={b.id}>
+                      <Book book={b} changeBookShelf={updateBookShelf} />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+    )
+}
+
+export default Search;
